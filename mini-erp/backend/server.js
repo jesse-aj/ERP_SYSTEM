c119155e3382d14a64e7e599aa0f812be56f3533
jesse-aj <appiahjesse123@gmail.com>
2026-04-25 08:07:58 +0000

    fix: update CORS for Vercel frontend

--- mini-erp/backend/server.js
+++ mini-erp/backend/server.js
@@ -14,7 +14,13 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://erp-system-eight-plum.vercel.app/'
+  ],
+  credentials: true
+}));
 
 // Routes
 app.use('/api/auth', authRoutes);
